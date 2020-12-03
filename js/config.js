b17252974dc05078b4b469d0eaac0d0af84819c0
Alexander <alexander.d.veit@gmail.com>
2020-12-02 20:07:25 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_ekXmJffHd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6o9pmcmt0dgr4m5q1oo3dn46le', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
